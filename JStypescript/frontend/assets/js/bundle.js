24842198846da97099fc7d02b3021e1162898181
Leonardo <treinamento-06@wks8n9yfv2.memora.com.br>
2022-07-15 15:51:46 -0300

    modificações feitas

---
+++ JStypescript/frontend/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!***********************************!*\
+  !*** ./src/inputwebpack/index.ts ***!
+  \***********************************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
